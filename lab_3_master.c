05aec3192e9237c4b342b1555ca7be3f9865e51b
krbriz <kristaps.brize@gmail.com>
2022-01-10 07:41:48 +0200

    1

--- lab_3_master.c
+++ lab_3_master.c
@@ -5,17 +5,14 @@
 
 void main()
 {
+    float a, b = 2*M_PI, x, delta_x = 1.e-2;
     printf("Ievadi X veertiibu:");
-    scanf(%d, &a);
-    float a=0, b = 2*M_PI, x, delta_x = 1.e-2;
-    // float y;
+    scanf("%d", &a);
     printf("\tx\t\tsin(x)\t\tsin\'(x)\n");
     x = a;
     while (x < b)
     {
-        // y=sin(x);
         printf("%10.2f\t%10.2f\t%10.2f\n", x, sin(x), (sin(x + delta_x) - sin(x)) / delta_x);
-        // printf(”%10.2f\t%10.2f\n”,x,y);
         x += delta_x; // x=x+delta_x;
     }
 }
